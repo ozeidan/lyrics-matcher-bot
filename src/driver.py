e9a2737d2c0cfd16be09be6b04276f45bf7985d4
Omar Zeidan <omar@zeidan.io>
2017-12-22 04:16:02 +0100

    Moved thread handling to driver file from main file

---
+++ src/driver.py
@@ -0,0 +1,31 @@
+from reddit_bot import CommentBot
+from lyrics_lookup import LyricsResolver
+import queue
+
+
+RESOLVER_THREAD_COUNT = 5
+
+snippet_queue = queue.Queue()
+lyrics_queue = queue.Queue()
+
+resolver_workers = []
+
+
+def start(subreddits, apikey):
+    CommentBot.snippet_queue = snippet_queue
+    CommentBot.lyrics_queue = lyrics_queue
+    LyricsResolver.snippet_queue = snippet_queue
+    LyricsResolver.lyrics_queue = lyrics_queue
+
+    for i in range(0, RESOLVER_THREAD_COUNT):
+        print("Starting worker {}".format(i))
+        worker = LyricsResolver(apikey)
+        worker.start()
+        resolver_workers.append(worker)
+
+    print("Starting comment parser/replier")
+    redditBot = CommentBot(subreddits, "bot1")
+    redditBot.start()
+
+    for worker in resolver_workers:
+        worker.thread.join()
