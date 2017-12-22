6656dc896acb1d6aa9997eb1489ac832cdb70c2f
Omar Zeidan <omar@zeidan.io>
2017-12-22 04:16:19 +0100

    Main file now handles argument parsing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
+import argparse
+import driver
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Start the lyrics matcher bot.")
+    parser.add_argument('apikey', metavar='apikey', nargs=1,
+                        help='API key for the genius.com API')
+    parser.add_argument('subreddits', metavar='subreddit', nargs='+',
+                        help='a list of subreddit names '
+                        'which the bot should listen')
+
+    args = parser.parse_args()
+    driver.start(args.subreddits, args.apikey[0])
